76a71fecad9f7b223e50e687e6bd8ca8eb4556aa
ronak <ronakpraja340@gmail.com>
2025-06-23 20:21:00 +0530

    routes use

--- BE/src/routes/auth.ts
+++ BE/src/routes/auth.ts
@@ -1,24 +1,22 @@
 import { Router } from "express";
 import { User } from "../db/db";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
 export const authRouter = Router();
 
-authRouter.post("/signup",async(req,res)=>{
-    const {username,firstName,lastName,password} = req.body
-    const hashedPassword = await bcrypt.hash(password,10)
-    try {
-        const user = new User({
-            username,
-            firstName,
-            lastName,
-            password:hashedPassword 
-        })
-        await user.save()
-        res.json({
-            msg : "user created succesfully!"
-        })
-    } catch (error) {
-        
-    }
-})
+authRouter.post("/signup", async (req, res) => {
+  const { username, firstName, lastName, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const user = new User({
+      username,
+      firstName,
+      lastName,
+      password: hashedPassword,
+    });
+    await user.save();
+    res.json({
+      msg: "user created succesfully!",
+    });
+  } catch (error) {}
+});

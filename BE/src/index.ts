52485f9f836d20f06d45cdfcadfef5f8144e2d6e
ronak <ronakpraja340@gmail.com>
2025-06-23 21:20:06 +0530

    env port

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from 'cors';
 import authRouter from "./routes/auth";
 import userRouter from "./routes/user";
+import { ENV } from "./config/env";
 
 const app = express();
 app.use(json());
@@ -12,7 +13,7 @@ app.use(cors({
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 
-app.listen(8080, () => {
+app.listen(ENV.PORT, () => {
   console.log("====================================");
   console.log("listening on the port 8080");
   console.log("====================================");

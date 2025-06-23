0b92ee1ff9df5e5dc883c7523185b548a44fcf6b
ronak <ronakpraja340@gmail.com>
2025-06-23 23:50:17 +0530

    authmiddleware done

---
+++ BE/src/middlewares/middleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { ENV } from "../config/env";
+
+export const authMiddleware = (req : Request,res:Response,next : NextFunction) => {
+    const authHeader = req.headers.authorization
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        res.status(403).json({
+            msg : "invalid token or input!"
+        })
+    }
+    //@ts-ignore
+    const token = authHeader.split(' ')[1];
+    try {
+        const decode = jwt.verify(token,ENV.JWT_SECRET)
+        //@ts-ignore
+        req.userId = decode.userId
+        next()
+    } catch (error) {
+        res.status(403).json({
+            msg : "Error while decoding token"
+        })
+        
+    }
+}

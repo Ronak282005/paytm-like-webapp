e85134003a242299fda93d755ae51d06f85985d3
ronak <ronakpraja340@gmail.com>
2025-06-23 22:39:33 +0530

    auth signup route done

--- BE/src/routes/auth.ts
+++ BE/src/routes/auth.ts
@@ -2,36 +2,36 @@ import { Router } from "express";
 import { User } from "../db/db";
 import bcrypt from "bcrypt";
 import { z } from "zod";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { ENV } from "../config/env";
 
 const authRouter = Router();
 
 const signupInput = z.object({
-    username : z.string(),
-    firstName : z.string(),
-    lastName : z.string(),
-    password : z.string().min(8)
-})
+  username: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+  password: z.string().min(8),
+});
 
 authRouter.post("/signup", async (req, res) => {
-    const {success} = signupInput.safeParse(req.body);
-    const { username, firstName, lastName, password } = req.body
-    if(!success){
-        res.status(403).json({
-            msg : "invalid inputs!"
-        })
+  const { success } = signupInput.safeParse(req.body);
+  const { username, firstName, lastName, password } = req.body;
+  if (!success) {
+    res.status(403).json({
+      msg: "invalid inputs!",
+    });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const alreadyUser = await User.findOne({
+      username,
+    });
+    if (alreadyUser) {
+      res.status(403).json({
+        msg: "Email already taken",
+      });
     }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    try {
-        const alreadyUser = await User.findOne({
-            username
-        })
-      if(alreadyUser){
-          res.status(403).json({
-              msg : "Email already taken"
-          })
-      }
     const user = new User({
       username,
       firstName,
@@ -39,12 +39,15 @@ authRouter.post("/signup", async (req, res) => {
       password: hashedPassword,
     });
     await user.save();
-    const token = jwt.sign({
-        userId : user._id
-    },ENV.JWT_SECRET);
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      ENV.JWT_SECRET
+    );
     res.json({
       msg: "user created succesfully!",
-      token
+      token,
     });
   } catch (error) {
     console.log(error);
@@ -54,4 +57,4 @@ authRouter.post("/signup", async (req, res) => {
   }
 });
 
-export default authRouter
+export default authRouter;

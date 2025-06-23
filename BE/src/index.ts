c419d693c3cbd79454b94a185261285e0e034b92
ronak <ronakpraja340@gmail.com>
2025-06-23 20:18:01 +0530

    routes use

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,27 +1,9 @@
 import express, { json } from "express";
-import { User } from "./db/db";
 
 const app = express()
 app.use(json())
+app.use("/api/v1/auth")
 
-app.get("/",(req,res)=>{
-    res.json({
-        msg : "Hello there"
-    })
-})
-
-app.post("/api/v1/signup",async(req,res)=>{
-    const {username,firstName,lastName,password} = req.body
-    try {
-        const user = await new User({
-            username,
-            firstName,
-            lastName,
-        })
-    } catch (error) {
-        
-    }
-})
 
 app.listen(8080,()=>{
     console.log('====================================');

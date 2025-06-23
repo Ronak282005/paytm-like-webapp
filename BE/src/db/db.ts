c2ed9aefe50564fbb5e00061f179d51a5f4abd33
ronak <ronakpraja340@gmail.com>
2025-06-23 18:03:52 +0530

    user schema and model done

--- BE/src/db/db.ts
+++ BE/src/db/db.ts
@@ -1,4 +1,27 @@
-import { connect } from "mongoose";
+import { connect, model, Schema } from "mongoose";
 import { ENV } from "../config/env";
 
-connect(ENV.DB_URL)
+connect(ENV.DB_URL);
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = model("User", userSchema);

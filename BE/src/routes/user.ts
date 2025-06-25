5f14468d6a44c2fd966bd3d19a17c59a9ec8b423
ronak <ronakpraja340@gmail.com>
2025-06-25 22:56:28 +0530

    /update route for the user

--- BE/src/routes/user.ts
+++ BE/src/routes/user.ts
@@ -5,27 +5,25 @@ import { authMiddleware } from "../middlewares/middleware";
 const userRouter = Router();
 
 const updateBody = z.object({
-    firstName : z.string().optional(),
-    lastName : z.string().optional(),
-    password : z.string().optional()
-})
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  password: z.string().optional(),
+});
 
-userRouter.post("/update",authMiddleware,async(req,res)=>{
-    const {success} = updateBody.safeParse(req.body)
-    if(!success){
-        res.status(403).json({
+userRouter.post("/update", authMiddleware, async (req, res) => {
+  const { success } = updateBody.safeParse(req.body);
+  if (!success) {
+    res.status(403).json({
       msg: "invalid inputs!",
     });
-    }
-    await User.updateOne(req.body,{
-        // @ts-ignore
-        id : req.userId
-    })
-    res.json({
-        msg : "Updated Successfully!"
-    })
-})
+  }
+  await User.updateOne(req.body, {
+    // @ts-ignore
+    id: req.userId,
+  });
+  res.json({
+    msg: "Updated Successfully!",
+  });
+});
 
-
-
-export default userRouter
+export default userRouter;
